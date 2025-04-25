602a497a436a002270ff2139a1f6bee32c179d5b
omidlmazaheri <omidlmazaheri@gmail.com>
2025-04-25 14:49:02 +0330

    creat a repo for js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let temperature = 25;
+if (temperature >= 30) {
+  alert("هوا گرم است!");
+} else if (temperature <=30 && temperature >= 10) {
+  alert("هوا معتدل است!");
+} else {
+  alert("هوا سرد است!");
+}
