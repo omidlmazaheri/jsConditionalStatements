58a53648a265a28ed568056fdc50991646fb48bb
omidlmazaheri <omidlmazaheri@gmail.com>
2025-04-25 15:02:14 +0330

    formated

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 let temperature = 25;
 if (temperature >= 30) {
-  alert("هوا گرم است!");
-} else if (temperature <=30 && temperature >= 10) {
-  alert("هوا معتدل است!");
+    alert("هوا گرم است!");
+} else if (temperature <= 30 && temperature >= 10) {
+    alert("هوا معتدل است!");
 } else {
-  alert("هوا سرد است!");
+    alert("هوا سرد است!");
 }
